1e7e7f3301db9dcfd2f1e4a820e92cb4b2f90791
Jack Atherton <jackrrye@gmail.com>
2019-06-15 18:38:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,17 @@
 class Song 
   
   @@songs = 0 
+  @@artists = []
+  @@genres = []
   
-  attr_accessor :name, :artist, :genre 
+  attr_reader :name, :artist, :genre 
   
   def initialize (name, artist, genre)
     @@songs += 1 
+    @@artists << artist
+    @@artists.uniq!
+    @@genres << genre 
+    @@genres.uniq!
   end
   
 end 
